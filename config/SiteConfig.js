974e2537a1d5fe77fed62dece792d9c9301765f2
LeKoArts <lekoarts@gmail.com>
2018-02-03 21:17:06 +0100

    Update

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
 
   siteTitle: 'Minimal Blog', // Navigation and Site Title
   siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: '...', // Domain of your site. No trailing slash!
+  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteBanner: '/social/banner.jpg',
