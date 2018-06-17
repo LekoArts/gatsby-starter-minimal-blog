1f97172233e571be0f4553b14ebc7bc9d116275c
LeKoArts <lekoarts@gmail.com>
2018-06-17 11:36:30 +0200

    children func to normal

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -68,7 +68,7 @@ const TemplateWrapper = props => {
     <ThemeProvider theme={theme}>
       <div>
         <SEO />
-        {children()}
+        {children}
         <Footer>
           &copy; 2018 by John Doe. All rights reserved. <br />
           <a href="https://github.com/LeKoArts/gatsby-starter-minimal-blog">GitHub Repository</a>
