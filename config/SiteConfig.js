efffba05f0df834a401616b5024a436240aa6ce6
LeKoArts <lekoarts@gmail.com>
2018-06-22 11:06:51 +0200

    Remove unused icons and removed favicon plugin as it doesn't seem to work anymore. Use the manifest-plugin instead

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -5,12 +5,11 @@ module.exports = {
   siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
   siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteBanner: '/social/banner.jpg',
   siteDescription: 'Minimal Blog with big typography',
 
   siteFBAppID: '123456789', // Facebook App ID
-  userTwitter: 'minimal', // Twitter Username
+  userTwitter: '@minimal', // Twitter Username
   ogSiteName: 'minimal', // Facebook Site Name
 
   // Manifest and Progress color
--- gatsby-config.js
+++ gatsby-config.js
@@ -52,36 +52,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'minimal-ui',
-        icons: [
-          {
-            src: '/logos/logo-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/logos/logo-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-favicon',
-      options: {
-        logo: './src/favicon.png',
-        injectHTML: true,
-        icons: {
-          android: false,
-          appleIcon: true,
-          appleStartup: false,
-          coast: true,
-          favicons: true,
-          firefox: false,
-          twitter: false,
-          yandex: false,
-          windows: true,
-        },
+        icon: 'src/favicon.png',
       },
     },
     'gatsby-plugin-offline',
