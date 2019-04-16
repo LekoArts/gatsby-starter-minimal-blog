b95cf23e5235cfb213c3383264f2cd28a9b5821f
Ben Parsons <ben@bpulse.co.uk>
2019-04-16 10:37:12 +0100

    add posts.js to mimic matrix.org/blog/posts

---
+++ src/pages/blog/posts.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
+import styled from 'styled-components'
+
+import { Layout, Header, Article, Wrapper, Button, SectionTitle } from '../../components'
+
+const Content = styled.div`
+  grid-column: 2;
+  box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  padding: 3rem 6rem;
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    padding: 3rem 2rem;
+  }
+  @media (max-width: ${props => props.theme.breakpoints.phone}) {
+    padding: 2rem 1.5rem;
+  }
+  overflow: hidden;
+`
+
+const Hero = styled.div`
+  grid-column: 2;
+  padding: 3rem 2rem 6rem 2rem;
+  text-shadow: 0 12px 30px rgba(0, 0, 0, 0.15);
+  color: ${props => props.theme.colors.grey.dark};
+
+  @media (max-width: ${props => props.theme.breakpoints.phone}) {
+    padding: 2rem 1rem 4rem 1rem;
+  }
+
+  p {
+    font-size: 1.68rem;
+    margin-top: -1rem;
+    @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+      font-size: 1.45rem;
+    }
+    @media (max-width: ${props => props.theme.breakpoints.phone}) {
+      font-size: 1.25rem;
+    }
+  }
+`
+
+const PostsPage = ({
+  data: {
+    allMdx: { edges: postEdges },
+  },
+}) => (
+  <Layout>
+    <Wrapper>
+      <Header>
+      </Header>
+      <Content>
+        <SectionTitle>Latest stories</SectionTitle>
+        {postEdges.map(post => (
+          <Article
+            title={post.node.frontmatter.title}
+            date={post.node.frontmatter.date}
+            excerpt={post.node.excerpt}
+            timeToRead={post.node.timeToRead}
+            slug={post.node.fields.slug}
+            categories={post.node.frontmatter.categories}
+            key={post.node.fields.slug}
+          />
+        ))}
+      </Content>
+    </Wrapper>
+  </Layout>
+)
+
+export default PostsPage
+
+PostsPage.propTypes = {
+  data: PropTypes.shape({
+    allMdx: PropTypes.shape({
+      edges: PropTypes.array.isRequired,
+    }),
+  }).isRequired,
+}
+
+export const PostsQuery = graphql`
+  query PostsQuery {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "MM/DD/YYYY")
+            categories
+          }
+          excerpt(pruneLength: 200)
+          timeToRead
+        }
+      }
+    }
+  }
+`
