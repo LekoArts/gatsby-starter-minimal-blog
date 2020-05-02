43ec53c624be260c968c9ecfdf2d1cde82e85582
LekoArts <lekoarts@gmail.com>
2020-05-02 13:13:04 +0200

    Revert "apply changes"
    This reverts commit c595453cdc0eb1c656087da557329dca1ab45ad5.

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-exports.onCreateNode = ({ actions, node, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === 'MdxPage') {
-    const fileNode = getNode(node.parent)
-    // console.log(fileNode)
-  }
-}
