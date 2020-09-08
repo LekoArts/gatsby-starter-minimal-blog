b42d04a3c6f23cd36b364ecc1023648499870753
Rajat Gupta <rajatgupta1798@gmail.com>
2020-09-08 19:01:15 +0530

    basic changes II

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,9 +73,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Rajat's blog`,
+        short_name: ``,
+        description: ``,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
