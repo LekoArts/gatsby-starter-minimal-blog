4a81e2770d6cc5ad4e145465363547d1d89dbceb
Marius Spahn <spahnmarius2@gmail.com>
2021-11-20 14:52:08 +0100

    Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitle: `Marius Spahn`,
+    siteTitleAlt: `Marius Spahn`,
+    siteHeadline: `Marius Spahn`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteLanguage: `en`,
