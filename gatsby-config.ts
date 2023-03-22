6c4fb7dbebff3b26c824a0403e36937aa2b7ceaf
Ankush Singh <ankushsmacbookair@Ankushs-MacBook-Air.local>
2023-03-23 01:44:26 +0530

    Added Twitter and LinkedIn

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,10 +8,10 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
+    siteTitle: `Ankush Singh`,
+    siteTitleAlt: `Ankush Singh`,
+    siteHeadline: `Ankush Singh - The Minimal Blog`,
+    siteUrl: `https://ankushsingh.in`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
@@ -36,11 +36,11 @@ const config: GatsbyConfig = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/_ankushsingh`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Linkedin`,
+            url: `https://www.linkedin.com/in/iamankushsingh/`,
           },
         ],
       },
